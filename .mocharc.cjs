d5d1b8878fb4069e3a40a4b808d28b966e1858c0
Myrotvorets <support@myrotvorets.center>
2023-09-21 05:32:56 +0300

    Update dependencies

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,13 +1,13 @@
 /** @type {import('mocha').MochaOptions} */
 module.exports = {
     recursive: true,
-    spec: ['test/**/*.test.mts'],
+    extension: ['.test.mts'],
     'node-option': ['loader=ts-node/esm', 'loader=testdouble', 'no-warnings'],
     require: 'mocha.setup.mjs',
     reporter: 'mocha-multi',
     'reporter-option': [
         'spec=-',
         process.env.GITHUB_ACTIONS === 'true' ? 'mocha-reporter-gha=-' : null,
-        'mocha-reporter-sonarqube=test-report.xml'
+        process.env.SONARSCANNER === 'true' ? 'mocha-reporter-sonarqube=test-report.xml' : null,
     ].filter(Boolean),
 }
