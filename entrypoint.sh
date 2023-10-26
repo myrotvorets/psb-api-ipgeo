61b35f98ee0f756e0ccbd90b5a623e6d64e9e1c3
Myrotvorets <support@myrotvorets.center>
2023-10-26 03:14:57 +0300

    Update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-set -x
 set -e
 
-[ -f /run/secrets/geoip-encryption-password ] || {
-    echo "FATAL: /run/secrets/geoip-encryption-password not found"
-    exit 1
-}
+KEYFILE=/run/secrets/geoip-encryption-password
 
-[ -f /usr/share/GeoIP/GeoIP2-City.mmdb ] || \
-    /usr/bin/openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in /usr/share/GeoIP/GeoIP2-City.mmdb.enc -out /usr/share/GeoIP/GeoIP2-City.mmdb -kfile /run/secrets/geoip-encryption-password
+if [ -f "${KEYFILE}" ]; then
+    if [ -n "${GEOIP_CITY_FILE}" ] && [ ! -f "${GEOIP_CITY_FILE}" ] && [ -f "${GEOIP_CITY_FILE}.enc" ]; then
+        /usr/bin/openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in "${GEOIP_CITY_FILE}.enc" -out "${GEOIP_CITY_FILE}" -kfile "${KEYFILE}"
+    fi
 
-
-[ -f /usr/share/GeoIP/GeoIP2-ISP.mmdb ] || \
-    /usr/bin/openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in /usr/share/GeoIP/GeoIP2-ISP.mmdb.enc -out /usr/share/GeoIP/GeoIP2-ISP.mmdb -kfile /run/secrets/geoip-encryption-password
-
-rm -f /run/secrets/geoip-encryption-password || true
+    if [ -n "${GEOIP_ISP_FILE}" ] && [ ! -f "${GEOIP_ISP_FILE}" ] && [ -f "${GEOIP_ISP_FILE}.enc" ]; then
+        /usr/bin/openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in "${GEOIP_ISP_FILE}.enc" -out "${GEOIP_ISP_FILE}" -kfile "${KEYFILE}"
+    fi
+fi
 
 exec /usr/bin/node index.mjs
